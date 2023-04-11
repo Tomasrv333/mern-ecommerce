53d1a626f4c0efc11f4898ff29c999f801788f80
Tomasrv333 <tomasrv123@gmail.com>
2023-04-11 17:02:29 -0500

    sass added

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <nav className='header__nav nav'>
+        <div className='nav__content'>
+            <h1 className='nav__title'>E-commerce</h1>
+            <div className='nav__input'>
+                <input></input>
+            </div>
+            <div className='nav__icons-content'>
+                
+            </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
